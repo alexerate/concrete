ad67fd386a17a1f8f94df33d9097e71a57bf11cb
alexerate <35458285+alexerate@users.noreply.github.com>
2018-01-29 03:29:35 +0100

    Add files via upload

--- tools.lua
+++ tools.lua
@@ -1,7 +1,7 @@
-minetest.register_tool("concrete:pick", {
-	description = "Concrete Pickaxe (admin)",
+minetest.register_tool("donj:pick", {
+	description = "Donj Pickaxe",
 	range = 12,
-	inventory_image = "concrete_pick.png",
+	inventory_image = "donj_pick.png",
 	--groups = {not_in_creative_inventory = maptools.creative},
 	tool_capabilities = {
 		full_punch_interval = 0.1,
@@ -14,7 +14,19 @@ minetest.register_tool("concrete:pick", {
 			cracky =      {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
 			crumbly =     {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
 			snappy =      {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			concrete =    {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
 		},
 		damage_groups = {fleshy = 1000},
 	},
 })
+
+
+--[[
+minetest.register_on_punchnode(function(pos, node, puncher)
+	if puncher:get_wielded_item():get_name() == "donj:pick"
+	and minetest.get_node(pos).name ~= "air" then
+		--minetest.log("action", puncher:get_player_name() .. " digs " .. minetest.get_node(pos).name .. " at " .. minetest.pos_to_string(pos) .. " using an Admin Pickaxe.")
+		minetest.remove_node(pos) -- The node is removed directly, which means it even works on non-empty containers and group-less nodes.
+		nodeupdate(pos) -- Run node update actions like falling nodes.
+	end
+end)]]--
